16af828038bebeb99a2383d4d4ad772b8236dd98
r6fans <wsyh19960325@gmail.com>
2023-09-21 11:55:29 +0800

    test

--- test.cpp
+++ test.cpp
@@ -1,4 +1,16 @@
 #include <stdio.h>
+
+void change(char ch[][3]){
+    ch[0][0] = 'n';
+}
+
 int main(){
-    printf("test");
+    char cs[2][3] = {
+        {'a','b','c'};
+        {'d','e','f'}
+    };
+    printf("cs[0][0] = %c\n", cs[][]);
+    change(cs);
+    printf("cs[0][0] = %c\n", cs[0][0]);
+    return 0;
 }
