93df8fce95791d2b79ed274d03fc87adf37e4af4
r6fans <wsyh1996325@126.com>
2023-11-01 17:34:02 +0800

    add some code

--- DataStruct/排序.cpp
+++ DataStruct/排序.cpp
@@ -108,7 +108,48 @@ void BuildMaxHeap(ElementType A[], int len){
 
 void HeadAdjust(ElementType A[],int k,int len){
     A[0]=A[K];                          //A[0]暂存子树的根节点
-    for(int i=2*k;i<=len;i*=2){         //
-        if(i<len&&A[i]<A[i+1])
+    for(int i=2*k;i<=len;i*=2){         //沿key较大的子节点向下筛选
+        if(i<len&&A[i]<A[i+1]){
+            i++;                        //沿key较大的子节点的下标
+        }
+        if(A[0]>=A[i]){
+            break;                      //筛选结束
+        }else{
+            A[k]=A[i];
+            k=i;                        //修改K值，以便继续向下筛选
+        }
     }
+    A[K]=A[0];                          //被筛选节点的值放入最终位置
+}
+
+void HeapSort(ElementType A[],int len){
+    BuildMaxHeap(A,len);                //初始建堆
+    for(int i=len;i>1;i--){             //n-1趟的交换和建堆过程
+        swap(A[i], A[1]);               //输出堆顶元素(和堆低元素交换)
+        HeadAdjust(A,1,i-1);            //调整,把剩余的i-1个元素整理成堆
+
+    }
+}
+
+void swap(int &a,int &b){
+    int t = a;
+    a = b;
+    b = t;
+}
+
+
+//归并排序
+ElementType *B = (ElementType *)malloc((n+1)*sizeof(ElementType)); //构造辅助数组B
+void Merge(ElementType A[],int low,int high){
+    //表A的两段数组A[low...mid]和A[mid+1...high]各自有序，将两个数组合并成一个有序数组
+    int i,j,k;
+    for(k=low;<=high;k++){
+        B[k]=A[k];                      //将A中所有元素复制到B中
+    }
+    for(i=low,j=mid+1,k=i;i<=mid&&j<=high;k++){
+        if(B[i]<=B[j])   A[K]=B[i++];   //比较B的左右两段中的元素
+        else    A[k]=B[j++];
+    }
+    while(i<=mid)   A[k++]=B[i++];      //若第一个表未检测完，复制
+    while(j<=high)  A[k++]=B[j++];      //若第二个表未检测完，复制
 }
